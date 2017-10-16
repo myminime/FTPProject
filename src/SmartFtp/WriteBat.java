c653228e9083d5edb2dfadadc62e7beae400765e
caxon27 <32257647+caxon27@users.noreply.github.com>
2017-10-16 16:34:06 +0700

    Update WriteBat.java
    bersih2

--- src/SmartFtp/WriteBat.java
+++ src/SmartFtp/WriteBat.java
@@ -18,36 +18,7 @@ import java.io.IOException;
 public class WriteBat {
     
     public static void main(String[] args) throws IOException {
-        
-        /*File newFile = new File("D:/Proyekan/omahanyar/sumber/exec.bat");
-        //File newFile = new File("C:/SmartMove/move.bat");
-        FileWriter wFile = new FileWriter(newFile);
-        Runtime runtime = Runtime.getRuntime();
-        
-        try (BufferedWriter bw = new BufferedWriter(wFile)) {
-            bw.write("set source_drive=");
-            bw.write("D:\\Proyekan\\omahanyar\\sumber"); //source path parameter diganti input text field
-            bw.write("\n");
-            bw.write("move %source_drive%\\*.txt ");
-            bw.write("D:\\Proyekan\\omahanyar\\tujuan"); //destination path parameter diganti input text field
-        }
-        System.out.println("file added");
-        
-        if (newFile.exists()) {
-            newFile.createNewFile();    
-        }
-        try {
-            newFile.createNewFile();
-            }
-        catch (IOException e){
-            }
-        try {
-            runtime.exec("cmd /c start D:\\Proyekan\\omahanyar\\sumber\\exec.bat");
-            //runtime.exec("timeout /t %3");
-            runtime.exec("taskkill /im cmd.exe /f");
-        }
-        catch (IOException e) {}*/
-        
+                
         ftpUI ui = new ftpUI();
         ui.setVisible(true);
         
